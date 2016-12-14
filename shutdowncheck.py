f8110616bf27dba1c543ac9e8565db1a74f8e264
Philip Mathieu <philip.eng.mathieu@gmail.com>
2016-12-13 19:38:18 -0500

    Reimplement using GPIO interrupts
    A cleaner way of dealing with waiting for the Sleepy Pi 2 to send the handshake pin high

--- shutdowncheck.py
+++ shutdowncheck.py
@@ -4,14 +4,15 @@ import RPi.GPIO as GPIO
 import os, time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(25, GPIO.OUT)
 GPIO.output(25, GPIO.HIGH)
 print ("[Info] Telling Sleepy Pi we are running pin 25")
 
-while True:
-	if (GPIO.input(24)):
-		print ("Sleepy Pi requesting shutdown on pin 24")
-		os.system("sudo shutdown -h now")
-		break
-	time.sleep(0.5)
+try:
+    GPIO.wait_for_edge(24, GPIO.RISING)
+    print "Rising edge detected on port 24. Here endeth the third lesson."
+    os.system("sudo shutdown -h now")
+except KeyboardInterrupt:
+    pass       
+GPIO.cleanup() # clean up GPIO on exit 
