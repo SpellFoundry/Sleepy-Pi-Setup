b92a2ba775380ce1ca2c1a0be9ef1ea2fc667711
Philip Mathieu <philip.eng.mathieu@gmail.com>
2016-12-13 19:40:24 -0500

    Correct print statements and remove unneeded time dependency

--- shutdowncheck.py
+++ shutdowncheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import RPi.GPIO as GPIO
-import os, time
+import os
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -11,8 +11,8 @@ print ("[Info] Telling Sleepy Pi we are running pin 25")
 
 try:
     GPIO.wait_for_edge(24, GPIO.RISING)
-    print "Rising edge detected on port 24. Here endeth the third lesson."
+    print ("Sleepy Pi requesting shutdown on pin 24")
     os.system("sudo shutdown -h now")
 except KeyboardInterrupt:
     pass       
-GPIO.cleanup() # clean up GPIO on exit 
+GPIO.cleanup()
